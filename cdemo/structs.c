9a442b76ba824c1163fcfaaa6cf87cec902db6e6
StudentFour <victoriachen71@gmail.com>
2018-06-08 16:09:53 -0700

    test

--- cdemo/structs.c
+++ cdemo/structs.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 #include <string.h>
 
@@ -21,7 +20,7 @@ void printStudent(struct Student* student)
 int main()
 {
 	char input[256];
-	printf("Would you like to add a new student? Respond with 1 for yes or 0 for no.\n");
+	printf("Add new student? Respond with 1 for yes or 0 for no.\n");
 	fgets(input, 256, stdin);
 	int yn = 2;
 	sscanf(input, "%d", &yn);
@@ -29,43 +28,42 @@ int main()
 	int studentnum = 0;
 		while (yn ==  1)
 		{
-				for (int i = studentnum; i < studentnum + 1; i++)
-				{
+		for (int i = studentnum; i < studentnum + 1; i++)
+		{
+		printf("Student First Name:\n");
+		fgets(input, 256, stdin);
+		char a[50];
+		sscanf(input, "%s", a);
+		strcpy(mystudent[i].firstnm, a);
 
-					printf("Student first name:\n");
-					fgets(input, 256, stdin);
-					char a[50];
-					sscanf(input, "%s", a);
-					strcpy(mystudent[i].firstnm, a);
-					
-					printf("Student last name:\n");
-                                        fgets(input, 256, stdin);
-					char b[50];
-                                        sscanf(input, "%s", b);
-					strcpy(mystudent[i].lastnm, b);
-					
-					printf("Student age:\n");
-                                        fgets(input, 256, stdin);
-					int c;
-                                        sscanf(input, "%d", &c);
-					mystudent[i].age = c;
+		printf("Student Last Name:\n");
+                fgets(input, 256, stdin);
+		char b[50];
+                sscanf(input, "%s", b);
+		strcpy(mystudent[i].lastnm, b);
 
-                                        printf("Student ID:\n");
-                                        fgets(input, 256, stdin);
-					int d;
-                                        sscanf(input, "%d", &d);
-					mystudent[i].studentid = d;
-				}
-			studentnum++;
-			printf("Add another student? 1=yes 0=no\n");
-			fgets(input, 256, stdin);
-			sscanf(input, "%d", &yn);
+		printf("Student Age:\n");
+                fgets(input, 256, stdin);
+		int c;
+                sscanf(input, "%d", &c);
+		mystudent[i].age = c;
+
+                printf("Student ID:\n");
+                fgets(input, 256, stdin);
+		int d;
+                sscanf(input, "%d", &d);
+		mystudent[i].studentid = d;
 		}
-	printf("Okay, here's the student list...\n");
+
+		studentnum++;
+		printf("Add another student? Respond with 1 for yes and 0 for no.\n");
+		fgets(input, 256, stdin);
+		sscanf(input, "%d", &yn);
+		}
+	printf("The student list is:\n");
 	for (int i = 0; i < studentnum; i++)
 	{
 		printStudent(&mystudent[i]);
-		//won't compile here: says "neither an array nor a pointer nor a vector"
 	}
 
 }
